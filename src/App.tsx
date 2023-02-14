fb86f683a39e603f6dda6d5227e0f75c0e201ddb
sangminlee98 <tkdals0975@naver.com>
2023-02-13 23:43:49 -0800

    feat : 프로젝트 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useRoutes } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { getClient } from './queryClient';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { routes } from '@/Routes';
+
+export default function App() {
+  const queryClient = getClient();
+  const elem = useRoutes(routes);
+  return (
+    <QueryClientProvider client={queryClient}>
+      {elem}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
