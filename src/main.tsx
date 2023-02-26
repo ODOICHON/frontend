f63b5ddf265426ac88de2f252849cc733e798f83
sangminlee98 <tkdals0975@naver.com>
2023-02-27 00:05:32 +0900

    chore : 폰트, 컬러 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { worker } from './mocks/worker';
+import './reset.scss';
 
 if (import.meta.env.DEV) {
   worker.start();
