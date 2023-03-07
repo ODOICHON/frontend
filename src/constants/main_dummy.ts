07f144d0ced3ebc980e20216e730ce0979b4bac7
sangminlee98 <tkdals0975@naver.com>
2023-03-07 22:45:53 +0900

    feat : 메인페이지 소개, 커뮤니티 UI 구현

---
+++ src/constants/main_dummy.ts
@@ -0,0 +1,133 @@
+import community1 from '@/assets/main/community1.png';
+import community2 from '@/assets/main/community2.png';
+import community3 from '@/assets/main/community3.png';
+import image1 from '@/assets/main/image1.png';
+import image2 from '@/assets/main/image2.png';
+import image3 from '@/assets/main/image3.png';
+import image4 from '@/assets/main/image4.png';
+import image5 from '@/assets/main/image5.png';
+import intro1 from '@/assets/main/intro1.svg';
+import intro2 from '@/assets/main/intro2.svg';
+
+export const communityData = [
+  {
+    title: `양평에서 좋은 매물을 얻는 법`,
+    content: `시골으로의 여행은 생각보다 시골으로의 여행은
+  여행은 생각보다 시골으로의 여행은생각보다 다.`,
+    imageUrl: community1,
+    date: new Date(2023, 0, 23),
+    author: 'wldkfjdkf',
+  },
+  {
+    title: `“시골에서의 운전은 숨쉬는 것과 같다.”`,
+    content: `시골으로의 여행은 생각보다 시골으로의 여행은
+  여행은 생각보다 시골으로의 여행은생각보다 다.`,
+    imageUrl: community2,
+    date: new Date(2023, 1, 23),
+    author: 'wldkfjdkf',
+  },
+  {
+    title: `아이 키우시는 분들  있으신가요?`,
+    content: `시골으로의 여행은 생각보다 시골으로의 여행은
+  여행은 생각보다 시골으로의 여행은생각보다 다.`,
+    imageUrl: community3,
+    date: new Date(2023, 1, 23),
+    author: 'wldkfjdkf',
+  },
+  {
+    title: `양평에서 좋은 매물을 얻는 법`,
+    content: `시골으로의 여행은 생각보다 시골으로의 여행은
+  여행은 생각보다 시골으로의 여행은생각보다 다.`,
+    imageUrl: community1,
+    date: new Date(2023, 1, 23),
+    author: 'wldkfjdkf',
+  },
+  {
+    title: `“시골에서의 운전은 숨쉬는 것과 같다.”`,
+    content: `시골으로의 여행은 생각보다 시골으로의 여행은
+  여행은 생각보다 시골으로의 여행은생각보다 다.`,
+    imageUrl: community2,
+    date: new Date(2023, 1, 23),
+    author: 'wldkfjdkf',
+  },
+  {
+    title: `아이 키우시는 분들  있으신가요?`,
+    content: `시골으로의 여행은 생각보다 시골으로의 여행은 
+  여행은 생각보다 시골으로의 여행은생각보다 다.`,
+    imageUrl: community3,
+    date: new Date(2023, 1, 23),
+    author: 'wldkfjdkf',
+  },
+];
+export const jumbotronData = [
+  {
+    title: `모든 가족들이 <strong>주말</strong> 만큼은<br/>세상에 <strong>단 하나뿐인 집</strong>에서 함께할 수 있는 <strong>그날</strong>까지`,
+    content: `주말의 집은 도시의 삶과 시골의 삶을 향유할 수 있는 듀얼라이프의 삶의 방식을 돕는 플랫폼입니다.
+  주말의 집은 빈집 중개를 통해 도심을 벗어난 사람들의 듀얼라이프 이상을 실현시킴에서
+  나아가 지역의 빈집 문제를 해결할 수 있습니다.`,
+    imageUrl: image1,
+    address: '경상남도 남해군 고현면 대곡리 114',
+  },
+  {
+    title: `모든 가족들이 <strong>주말</strong> 만큼은<br/>세상에 <strong>단 하나뿐인 집</strong>에서 함께할 수 있는 <strong>그날</strong>까지`,
+    content: `주말의 집은 도시의 삶과 시골의 삶을 향유할 수 있는 듀얼라이프의 삶의 방식을 돕는 플랫폼입니다.
+  주말의 집은 빈집 중개를 통해 도심을 벗어난 사람들의 듀얼라이프 이상을 실현시킴에서
+  나아가 지역의 빈집 문제를 해결할 수 있습니다.`,
+    imageUrl: image2,
+    address: '경상남도 남해군 고현면 대곡리 114',
+  },
+  {
+    title: `모든 가족들이 <strong>주말</strong> 만큼은<br/>세상에 <strong>단 하나뿐인 집</strong>에서 함께할 수 있는 <strong>그날</strong>까지`,
+    content: `주말의 집은 도시의 삶과 시골의 삶을 향유할 수 있는 듀얼라이프의 삶의 방식을 돕는 플랫폼입니다.
+  주말의 집은 빈집 중개를 통해 도심을 벗어난 사람들의 듀얼라이프 이상을 실현시킴에서
+  나아가 지역의 빈집 문제를 해결할 수 있습니다.`,
+    imageUrl: image3,
+    address: '경상남도 남해군 고현면 대곡리 114',
+  },
+  {
+    title: `모든 가족들이 <strong>주말</strong> 만큼은<br/>세상에 <strong>단 하나뿐인 집</strong>에서 함께할 수 있는 <strong>그날</strong>까지`,
+    content: `주말의 집은 도시의 삶과 시골의 삶을 향유할 수 있는 듀얼라이프의 삶의 방식을 돕는 플랫폼입니다.
+  주말의 집은 빈집 중개를 통해 도심을 벗어난 사람들의 듀얼라이프 이상을 실현시킴에서
+  나아가 지역의 빈집 문제를 해결할 수 있습니다.`,
+    imageUrl: image4,
+    address: '경상남도 남해군 고현면 대곡리 114',
+  },
+  {
+    title: `모든 가족들이 <strong>주말</strong> 만큼은<br/>세상에 <strong>단 하나뿐인 집</strong>에서 함께할 수 있는 <strong>그날</strong>까지`,
+    content: `주말의 집은 도시의 삶과 시골의 삶을 향유할 수 있는 듀얼라이프의 삶의 방식을 돕는 플랫폼입니다.
+  주말의 집은 빈집 중개를 통해 도심을 벗어난 사람들의 듀얼라이프 이상을 실현시킴에서
+  나아가 지역의 빈집 문제를 해결할 수 있습니다.`,
+    imageUrl: image5,
+    address: '경상남도 남해군 고현면 대곡리 114',
+  },
+];
+export const odoiIntroData = [
+  {
+    category: `트랜드`,
+    title: `듀얼 라이프 생활 백서 : 경제편`,
+    content: `경제적인 부담으로 듀얼라이프를 망설이셨나요?
+  듀얼 라이프를 위한 지원금 및 정책을 알려드려요.`,
+    image: intro1,
+  },
+  {
+    category: `트랜드`,
+    title: `듀얼 라이프 생활 백서 : 경제편`,
+    content: `경제적인 부담으로 듀얼라이프를 망설이셨나요?
+  듀얼 라이프를 위한 지원금 및 정책을 알려드려요.`,
+    image: intro2,
+  },
+  {
+    category: `트랜드`,
+    title: `듀얼 라이프 생활 백서 : 경제편`,
+    content: `경제적인 부담으로 듀얼라이프를 망설이셨나요?
+  듀얼 라이프를 위한 지원금 및 정책을 알려드려요.`,
+    image: intro1,
+  },
+  {
+    category: `트랜드`,
+    title: `듀얼 라이프 생활 백서 : 경제편`,
+    content: `경제적인 부담으로 듀얼라이프를 망설이셨나요?
+  듀얼 라이프를 위한 지원금 및 정책을 알려드려요.`,
+    image: intro2,
+  },
+];
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,16 +1,30 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import styles from './styles.module.scss';
 import { Autoplay, Pagination, Navigation } from 'swiper';
-import { jumbotronData } from '@/constants/jumbotron';
+import {
+  communityData,
+  jumbotronData,
+  odoiIntroData,
+} from '@/constants/main_dummy';
 import pin from '@/assets/common/map-pin.svg';
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
 
 export default function MainPage() {
+  const [_, updateState] = useState(false);
+  const introNextRef = useRef<HTMLButtonElement>(null);
+  const introPrevRef = useRef<HTMLButtonElement>(null);
+  const commuNextRef = useRef<HTMLButtonElement>(null);
+  const commuPrevRef = useRef<HTMLButtonElement>(null);
+  useEffect(() => {
+    updateState(true);
+  }, [introNextRef, commuNextRef]);
   return (
     <div className={styles.container}>
       <Swiper
@@ -35,7 +49,8 @@ export default function MainPage() {
           url(${data.imageUrl})`,
             }}
           >
-            <h1>{data.title}</h1>
+            <h3>0{idx + 1}</h3>
+            <h1 dangerouslySetInnerHTML={{ __html: data.title }}></h1>
             <p>{data.content}</p>
             <div className={styles.address}>
               <img src={pin} alt="map-pin" />
@@ -44,6 +59,114 @@ export default function MainPage() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className={styles.section1}>
+        <div className={styles.odoiIntro}>
+          <h3>오도이촌 소개</h3>
+          <div className={styles.odoiIntro_title}>
+            <h1>도시와 농촌을 오가는 라이프 스타일,</h1>
+            <h1>오도이촌에 대한 다양한 소식을</h1>
+            <h1>지금 만나보세요.</h1>
+          </div>
+          <div className={styles.odoiIntro_buttons}>
+            <button>트랜드</button>
+            <button className={styles.button_disabled}>후기</button>
+          </div>
+          <div className={styles.odoiIntro_bottom}>
+            <Link to="#">더 알아보기</Link>
+            <div className={styles.slideButton}>
+              <button ref={introPrevRef}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M9.125 21.1L.7 12.7q-.15-.15-.213-.325T.425 12q0-.2.063-.375T.7 11.3l8.425-8.425q.35-.35.875-.35t.9.375q.375.375.375.875t-.375.875L3.55 12l7.35 7.35q.35.35.35.863t-.375.887q-.375.375-.875.375t-.875-.375Z"
+                  />
+                </svg>
+              </button>
+              <button ref={introNextRef}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M7.15 21.1q-.375-.375-.375-.888t.375-.887L14.475 12l-7.35-7.35q-.35-.35-.35-.875t.375-.9q.375-.375.888-.375t.887.375l8.4 8.425q.15.15.213.325T17.6 12q0 .2-.063.375t-.212.325L8.9 21.125q-.35.35-.863.35T7.15 21.1Z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <Swiper
+          slidesPerView={2}
+          navigation={{
+            nextEl: introNextRef.current,
+            prevEl: introPrevRef.current,
+          }}
+          modules={[Navigation]}
+          className={styles.sectionSwiper}
+        >
+          {odoiIntroData.map((data, idx) => (
+            <SwiperSlide key={idx}>
+              <div className={styles.odoiTrend}>
+                <div className={styles.odoiTrend_text}>
+                  <h1>{data.category}</h1>
+                  <div>
+                    <h3>{data.title}</h3>
+                    <p>{data.content}</p>
+                  </div>
+                </div>
+                <img className={styles.odoiTrend_image} src={data.image} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className={styles.section2}>
+        <h3>오도이촌 커뮤니티</h3>
+        <h1>{`‘오도이촌’이라는 공통의 관심사를 가진 분들과
+다양한 이야기를 공유해보세요.`}</h1>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={{
+            nextEl: commuNextRef.current,
+            prevEl: commuPrevRef.current,
+          }}
+          modules={[Pagination, Navigation]}
+          className={styles.communitySwiper}
+        >
+          {communityData.map((data, idx) => (
+            <SwiperSlide key={idx}>
+              <div
+                className={styles.odoiCommunity}
+                style={{
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.35)),
+                url(${data.imageUrl})`,
+                }}
+              >
+                <h1>{data.title}</h1>
+                <p>{data.content}</p>
+                <div className={styles.commuMeta}>
+                  <p>{dayjs(data.date).format('YYYY-MM-DD')}</p>
+                  <p>{data.author}님</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button ref={commuPrevRef}>prev</button>
+        <button ref={commuNextRef}>next</button>
+      </div>
     </div>
   );
 }
