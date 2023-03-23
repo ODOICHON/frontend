71d3c00e7d29787737e694eade3d7f83f11d1644
sangminlee98 <tkdals0975@naver.com>
2023-03-23 19:28:48 +0900

    feat : Login API 연결 (에러처리 전)
    zustand persist를 사용하여 토큰관리

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,44 @@
 import removeImg from '@/assets/common/remove.png';
 import { opacityVariants } from '@/constants/variants';
 import useInput from '@/hooks/useInput';
+import { restFetcher } from '@/queryClient';
+import userStore from '@/store/userStore';
+import { useMutation } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './styles.module.scss';
 
+type LoginForm = {
+  email: string;
+  password: string;
+};
+
 export default function LoginPage() {
+  const { mutate: loginAPI } = useMutation(
+    (form: LoginForm) =>
+      restFetcher({
+        method: 'POST',
+        path: '/api/v1/users/sign-in',
+        body: form,
+      }),
+    {
+      onSuccess: (res) => {
+        if (!res || res.code !== 'SUCCESS') throw Error;
+        const tokens: Tokens = {
+          access_token: res.data.access_token,
+          refresh_token: res.data.refresh_token,
+        };
+        setTokens(tokens);
+        navigate('/');
+      },
+      onError(err) {
+        console.log(err);
+      },
+    },
+  );
+  const { setTokens } = userStore();
+  const navigate = useNavigate();
   const [id, handleId, setId] = useInput('');
   const [password, handlePassword, setPassword] = useInput('');
   const handleRemove = (e: React.SyntheticEvent) => {
@@ -13,6 +46,17 @@ export default function LoginPage() {
     if (e.currentTarget.parentElement?.id === 'passwordContainer')
       setPassword('');
     else return;
+  };
+  const handleLogin = () => {
+    if (id === '' || password === '') {
+      alert('아이디 또는 비밀번호를 입력하세요.');
+      return;
+    }
+    const form: LoginForm = {
+      email: id,
+      password,
+    };
+    loginAPI(form);
   };
   return (
     <motion.div
@@ -55,7 +99,9 @@ export default function LoginPage() {
             />
           )}
         </div>
-        <button className={styles.loginButton}>로그인</button>
+        <button className={styles.loginButton} onClick={handleLogin}>
+          로그인
+        </button>
         <span className={styles.subContainer}>
           <p>회원으로 가입하고 싶으신가요?</p>
           <Link to="/signup">회원가입</Link>
