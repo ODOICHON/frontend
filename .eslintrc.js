5988c1e1e5ec691bb17bdefa4aa52c88d32a4805
sangminlee98 <tkdals0975@naver.com>
2023-04-29 04:26:03 +0900

    chore : husky 설정 및 eslint rule 추가
    - 'no-var': 'error', // var 금지
    - 'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
    - 'no-console': ['error', { allow: ['warn', 'error', 'info'] }], // console.log() 금지
    - 'dot-notation': 'error', // 가능하다면 dot notation 사용
    - 'no-unused-vars': 'error', // 사용하지 않는 변수 금지
    
    - pre-commit: lint-staged가 실행
    - pre-push: lint가 실행

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'on',
     '@typescript-eslint/explicit-module-boundary-types': 'on',
     '@typescript-eslint/no-explicit-any': 'on',
+    'no-var': 'error', // var 금지
+    'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }], // console.log() 금지
+    'dot-notation': 'error', // 가능하다면 dot notation 사용
+    'no-unused-vars': 'error', // 사용하지 않는 변수 금지
   },
 };
