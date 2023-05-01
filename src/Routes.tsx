2710b47ae7680f3e211e1e0e3a2204bd6b6a5284
sangminlee98 <tkdals0975@naver.com>
2023-05-01 16:43:43 +0900

    feat : 커뮤니티 라우팅 구조 설계

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,5 @@
 import { lazy } from 'react';
+import { RouteObject } from 'react-router-dom';
 import GlobalLayout from '@/pages/_layout';
 
 const MainPage = lazy(() => import('@/pages/Main'));
@@ -6,12 +7,16 @@ const LoginPage = lazy(() => import('@/pages/Login'));
 const SignUpPage = lazy(() => import('@/pages/SignUp'));
 const MyPage = lazy(() => import('@/pages/Mypage'));
 const IntroducePage = lazy(() => import('@/pages/Introduce'));
-const TradePage = lazy(() => import('@/pages/Trade'));
-const CommunityPage = lazy(() => import('@/pages/Community'));
 const IntroWritePage = lazy(() => import('@/pages/IntroWrite'));
 const IntroBoardPage = lazy(() => import('@/pages/IntroBoard'));
+const CommunityPage = lazy(() => import('@/pages/Community'));
+const FreeBoardPage = lazy(() => import('@/pages/Community/FreeBoard'));
+const AdvertisementBoard = lazy(
+  () => import('@/pages/Community/AdvertisementBoard'),
+);
+const TradePage = lazy(() => import('@/pages/Trade'));
 
-export const routes = [
+export const routes: RouteObject[] = [
   {
     path: '/',
     element: <GlobalLayout />,
@@ -22,7 +27,20 @@ export const routes = [
       { path: '/mypage', element: <MyPage /> },
       { path: '/introduce', element: <IntroducePage /> },
       { path: '/trade', element: <TradePage /> },
-      { path: '/community', element: <CommunityPage /> },
+      {
+        path: '/community',
+        element: <CommunityPage />,
+        children: [
+          {
+            path: '/community/free_board',
+            element: <FreeBoardPage />,
+          },
+          {
+            path: '/community/advertisement_board',
+            element: <AdvertisementBoard />,
+          },
+        ],
+      },
       { path: '/intro_write', element: <IntroWritePage /> },
       { path: '/intro_board/:id', element: <IntroBoardPage /> },
     ],
--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -1,11 +1,42 @@
+import { useEffect } from 'react';
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import Preparation from '@/components/Preparation';
 import { opacityVariants } from '@/constants/variants';
+import styles from './styles.module.scss';
 
 export default function CommunityPage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isFocus = (menu: string) => {
+    const path = location.pathname.split('/')[2];
+    if (path && path.includes(menu)) {
+      return styles.focusMenu;
+    }
+    return styles.menu;
+  };
+  useEffect(() => {
+    navigate('/community/free_board');
+  }, []);
   return (
     <motion.div variants={opacityVariants} initial="initial" animate="mount">
-      <Preparation />
+      <nav className={styles.container}>
+        <div className={styles.innerContainer}>
+          <p>커뮤니티</p>
+          <div className={styles.divider} />
+          <span className={styles.menuWrapper}>
+            <Link className={isFocus('free')} to="/community/free_board">
+              자유게시판
+            </Link>
+            <Link
+              className={isFocus('advertisement')}
+              to="/community/advertisement_board"
+            >
+              홍보게시판
+            </Link>
+          </span>
+        </div>
+      </nav>
+      <Outlet />
     </motion.div>
   );
 }
