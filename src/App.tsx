3a0b105738b41394973a1c0868739c22349ac9f0
sangminlee98 <tkdals0975@naver.com>
2023-05-24 16:33:02 +0900

    refact: axios interceptor 코드 커스텀훅으로 작성하여 컴포넌트와 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,12 @@
-import { useEffect } from 'react';
-import { useLocation, useRoutes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import axios from 'axios';
 import { routes } from '@/Routes';
-import { reissue } from './apis/reissue';
 import { getClient } from './queryClient';
-import userStore from './store/userStore';
-import { setInterceptor } from './utils/utils';
 
 export default function App() {
-  const { pathname } = useLocation();
-  const { token, setToken, logout } = userStore();
-  const { VITE_BASE_URL } = import.meta.env;
-
-  axios.defaults.baseURL = VITE_BASE_URL;
-  let refresh = false;
-  axios.interceptors.response.use(
-    (res) => res,
-    async (error) => {
-      if (error.response?.status === 401 && !refresh) {
-        try {
-          refresh = true;
-          const data = await reissue();
-          setToken({
-            access_token: data!.data.access_token,
-          });
-          axios.defaults.headers.common.Authorization = data!.data.access_token;
-          error.config.headers.authorization = data!.data.access_token;
-          refresh = false;
-          return axios.request(error.config);
-        } catch (e) {
-          delete axios.defaults.headers.common.Authorization;
-          logout();
-        }
-      }
-      refresh = false;
-      return Promise.reject(error);
-    },
-  );
-
   const queryClient = getClient();
   const elem = useRoutes(routes);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  useEffect(() => {
-    if (token) {
-      setInterceptor(token.access_token);
-    }
-  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
