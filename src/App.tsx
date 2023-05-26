f92fb94b35d5c05a49c7bcb52fb5884603cf0a8f
sangminlee98 <tkdals0975@naver.com>
2023-05-27 04:33:58 +0900

    chore: React GA Initialize 코드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import ReactGA from 'react-ga';
 import { useRoutes } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { routes } from '@/Routes';
 import { getClient } from './queryClient';
 
+const GA_TRACK_ID = import.meta.env.VITE_GA_TRACK_ID;
+
 export default function App() {
   const queryClient = getClient();
   const elem = useRoutes(routes);
+
+  ReactGA.initialize(GA_TRACK_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
     <QueryClientProvider client={queryClient}>
