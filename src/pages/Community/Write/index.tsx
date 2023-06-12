560d363df4f773962b7f619030197eec0a817a36
jonghun <minn4072@gmail.com>
2023-06-12 22:00:05 +0900

    fix: 뒤로가기 버그 수정
    로그인이 안된 상태에서 커뮤니티 페이지 글쓰기 버튼 누르면 로그인 페이지로 이동, 로그인 페이지에서 뒤로가기 버튼 누르면 안 먹힘 해결

--- src/pages/Community/Write/index.tsx
+++ src/pages/Community/Write/index.tsx
@@ -1,16 +1,12 @@
 import { Navigate, useParams } from 'react-router-dom';
 import CommunityQuill from '@/components/Community/Quill';
-import userStore from '@/store/userStore';
 
 function CommunityWritePage() {
   const { category } = useParams();
-  const { token } = userStore();
+
   if (category !== 'free_board' && category !== 'advertisement_board')
     return <Navigate to="/community/free_board" />;
 
-  if (!token) {
-    return <Navigate to="/login" />;
-  }
   return <CommunityQuill queryParam={category} />;
 }
 
