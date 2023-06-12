a501146876609b2d83f27835e18517b92a41e860
jonghun <minn4072@gmail.com>
2023-06-12 22:42:12 +0900

    fix: token 여부 확인

--- src/pages/Community/Write/index.tsx
+++ src/pages/Community/Write/index.tsx
@@ -1,8 +1,12 @@
 import { Navigate, useParams } from 'react-router-dom';
 import CommunityQuill from '@/components/Community/Quill';
+import userStore from '@/store/userStore';
 
 function CommunityWritePage() {
   const { category } = useParams();
+  const { token } = userStore();
+
+  if (!token) return <Navigate to="/login" />;
 
   if (category !== 'free_board' && category !== 'advertisement_board')
     return <Navigate to="/community/free_board" />;
