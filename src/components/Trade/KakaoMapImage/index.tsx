5c9e1cb9a18c08da3ebcdd63f232d7f4d9cbb078
jonghun <minn4072@gmail.com>
2023-06-30 05:44:47 +0900

    feat: KakaoMapImage 컴포넌트 props 추가

--- src/components/Trade/KakaoMapImage/index.tsx
+++ src/components/Trade/KakaoMapImage/index.tsx
@@ -1,21 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import { StaticMap } from 'react-kakao-maps-sdk';
+import { getLatLng } from '@/utils/utils';
 import styles from './styles.module.scss';
 
-// type KakaoMapImageProps = {
-//   position: { lat: number; lng: number };
-// };
+type KakaoMapImageProps = {
+  address: string;
+};
 
-function KakaoMapImage() {
-  // 카카오 api를 이용하여 주소를 좌표로 변환
+function KakaoMapImage({ address }: KakaoMapImageProps) {
+  const [position, setPosition] = useState({ lat: 0, lng: 0 });
+
+  useEffect(() => {
+    getLatLng(address).then((res) => {
+      setPosition(res);
+    });
+  }, [address]);
 
   return (
-    <StaticMap // 지도를 표시할 Container
-      className={styles.KakaoMapImage}
-      center={{ lat: 33.450701, lng: 126.570667 }}
-      marker={{ position: { lat: 33.450701, lng: 126.570667 } }} // 마커가 표시될 위치
-      level={3} // 지도의 확대 레벨
-    />
+    <section className={styles.kakaoMapImage}>
+      <StaticMap // 지도를 표시할 Container
+        style={{ width: '100%', height: '100%' }}
+        center={position}
+        marker={{ position }} // 마커가 표시될 위치
+        level={3} // 지도의 확대 레벨
+      />
+    </section>
   );
 }
 
-export default KakaoMapImage;
+export default React.memo(KakaoMapImage);
