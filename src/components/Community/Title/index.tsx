45f58ddcd8e87f009477b1ddad9afa4e88f7dbe9
sangminlee98 <tkdals0975@naver.com>
2023-08-23 15:26:50 +0900

    fix: 커뮤니티 글쓰기 제목 수정

--- src/components/Community/Title/index.tsx
+++ src/components/Community/Title/index.tsx
@@ -6,7 +6,7 @@ type TitleProps = {
 };
 
 export default function Title({ category, boardData }: TitleProps) {
-  const title = category === 'free' ? '자유게시판' : '홍보게시판';
+  const title = category === 'free_board' ? '자유게시판' : '홍보게시판';
 
   return <h1>{boardData ? `${title} 수정하기` : `${title} 글쓰기`}</h1>;
 }
