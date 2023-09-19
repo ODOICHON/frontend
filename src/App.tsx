7e0cffc1ae2df88f6db5cdaea4114942fdeaf025
sangminlee98 <tkdals0975@naver.com>
2023-09-19 15:58:40 +0900

    chore: 구글 태그매니저 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { routes } from '@/Routes';
 import useReactGA from './hooks/useReactGA';
+import useReactGTM from './hooks/useReactGTM';
 import { getClient } from './queryClient';
 
 export default function App() {
   useReactGA();
+  useReactGTM();
 
   const queryClient = getClient();
   const elem = useRoutes(routes);
