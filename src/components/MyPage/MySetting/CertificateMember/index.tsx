35d9e91e9f064ee947c34291a15bff2c3d7c8ce8
sangminlee98 <tkdals0975@naver.com>
2023-12-15 22:11:10 +0900

    feat: 회원정보수정 페이지 비밀번호 인증 컴포넌트 UI

--- src/components/MyPage/MySetting/CertificateMember/index.tsx
+++ src/components/MyPage/MySetting/CertificateMember/index.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+import eyeImage from '@/assets/common/eye.svg';
+import eyeClosedImage from '@/assets/common/eyeClosed.svg';
 import { SettingStep } from '@/types/MyPage/settingType';
+import useInput from '@/hooks/useInput';
+import styles from './styles.module.scss';
 
 type CertificateMemberProps = {
   setSettingStep: React.Dispatch<React.SetStateAction<SettingStep>>;
@@ -7,6 +12,38 @@ type CertificateMemberProps = {
 export default function CertificateMember({
   setSettingStep,
 }: CertificateMemberProps) {
-  console.log(setSettingStep);
-  return <div>비밀번호 인증</div>;
+  const [password, handlePassword] = useInput('');
+  const [eyeCheckState, setEyeCheckState] = useState(false);
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputContainer}>
+        <label htmlFor="password">현재 비밀번호</label>
+        <input
+          className={styles.inputStyle}
+          id="password"
+          type={eyeCheckState ? 'text' : 'password'}
+          value={password}
+          onChange={handlePassword}
+          placeholder="현재 비밀번호를 입력하세요."
+        />
+        {password !== '' && (
+          <img
+            role="presentation"
+            id="password_checkEye"
+            className={styles.eyeImage}
+            src={eyeCheckState ? eyeClosedImage : eyeImage}
+            onClick={() => setEyeCheckState((prev) => !prev)}
+            alt="eyeImage"
+          />
+        )}
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => setSettingStep('editInfo')}
+        >
+          확인
+        </button>
+      </div>
+    </div>
+  );
 }
